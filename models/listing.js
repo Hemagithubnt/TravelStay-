75e496c4bf9cf69161c2760bb48857662d4578bc
Hemant Kumar <hmt364134@gmail.com>
2025-07-15 02:53:37 -0700

    First Commit

---
+++ models/listing.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const imageSchema = new Schema({
+  filename: {
+    type: String,
+    default: "default.jpg"
+  },
+  url: {
+    type: String,
+    default: "https://images.unsplash.com/photo-1622396481328-9b1b78cdd9fd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c2t5JTIwdmFjYXRpb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60"
+  }
+});
+
+const listingSchema = new Schema({
+  title: String,
+  description: String,
+  image: imageSchema,  
+  price: Number,
+  location: String,
+  country: String,
+  reviews: [{
+    type: Schema.Types.ObjectId,
+    ref: "Review"
+  }
+
+  ]
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
+
